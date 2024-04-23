a633d5354e9064664fa7e4e55b06c902f2a5feeb
leticia794 <129951386+leticia794@users.noreply.github.com>
2024-04-23 10:25:56 -0300

    Update fatorial.js

--- fatorial.js
+++ fatorial.js
@@ -4,3 +4,17 @@ resultado = fatorial(fatorial-1)*(fatorial-2)*(fatorial-3)*(fatorial-4)
 
 console.log("o resultado é: " + resultado);
 
+
+
+ const fatorial = 345
+let i = 0;
+let resultado = 0;
+
+resultado = fatorial;
+console.log(resultado + "!");
+for(i = 0; i< fatorial; i++ ){
+    console.log(resultado + " = "+resultado+"*("+fatorial+"-" + i + ")");
+resultado = resultado * fatorial *(fatorial - i); 
+}
+
+
